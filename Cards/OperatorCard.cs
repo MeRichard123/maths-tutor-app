238bc7635a1433240e895eeb1c3a2f994688ec79
Richard <rcoric03@gmail.com>
2023-04-01 17:51:50 +0100

    Create Files

---
+++ Cards/OperatorCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsTutor.Cards
+{
+    internal class OperatorCard: Card
+    {
+
+
+        public override string Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
