59f38dbd04bbb269614a7ffcd734da81cd9bd98e
Richard <rcoric03@gmail.com>
2023-04-27 16:20:48 +0100

    Fix typos

--- Tutorial.cs
+++ Tutorial.cs
@@ -7,7 +7,7 @@
             "Hello Fellow Maths Enthusiasts!",
             "Welcome to your maths tutor! ",
             "- You will be presented with a menu on each round",
-            "- It it your choice whether you decide on 3 cards of if you are feeling up for the challenge 5 cards.",
+            "- It is your choice whether you decide on 3 cards or if you are feeling up for the challenge 5 cards.",
             "- For each one calcualte the correct answer and enter it in the box.",
             "- If you pick 5 remember BODMAS",
             "GOOD LUCK!! \n",
